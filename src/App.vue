c146ea6519a7942ee41bd599158ea572a057e07c
juan <juanpablogomezzapata@gmail.com>
2020-06-15 16:30:39 -0400

    body human

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <front/>
+    <back/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import front from './components/front.vue'
+import back from './components/back.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    front,
+    back
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
